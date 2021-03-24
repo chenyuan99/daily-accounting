75f80a60ae2e4c09654752bfec96b517ac7c52c8
Yuan Chen <18861720589@163.com>
2021-03-24 09:41:09 -0400

    Update urls.py

--- rdie/urls.py
+++ rdie/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('accounting/', include('accounting.urls')),
+    path('', include('accounting.urls')),
     path('admin/', admin.site.urls),
 ]
